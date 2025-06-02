4f3d8af51ab41c1f71c16b0f1dd660e7deed4a1c
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2025-06-03 00:15:09 +0700

    rev_lagi

--- server.php
+++ server.php
@@ -1,7 +1,8 @@
 <?php
 
-$port = $_ENV['PORT'] ?? 8000;
+$port = getenv('PORT') ?: 8000; // Railway kasih PORT sendiri, lokal fallback ke 8000
 
-echo "Starting Laravel on port $port...\n";
+echo "Starting Laravel on port {$port}...\n";
 
+// Jalankan Laravel dengan host 0.0.0.0 dan port yang benar
 passthru("php artisan serve --host=0.0.0.0 --port={$port}");
